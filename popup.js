579a42069e203e573244762e2a6db2cddf17ae49
Alagon <alagon@deeplay.io>
2025-06-26 17:17:09 +0600

    Версия 6

--- popup.js
+++ popup.js
@@ -2,6 +2,9 @@
 const button = document.getElementById('grabBtn');
 const button2 = document.getElementById('grabBtn2');
 const button3 = document.getElementById('grabBtn3');
+const button4 = document.getElementById('grabBtn4');
+const button5 = document.getElementById('grabBtn5');
+const button6 = document.getElementById('grabBtn6');
 
 
 //Кнопка для Headless
@@ -59,7 +62,7 @@ button2.addEventListener('click', function() {
         });
 
     
-// Кнопка мозговых логов
+// Кнопка мозговых логов по раздаче
 button3.addEventListener('click', function() {
     extractUrlParams(([gameId, accId, gameDate, brainServer]) => {
         const accIdnumber = Number(accId);
@@ -79,6 +82,73 @@ button3.addEventListener('click', function() {
     });
   });
 
+// Кнопка мозговых логов на открытие стола
+button4.addEventListener('click', function() {
+    extractUrlParams(([gameId, accId, gameDate, brainServer, dateOpenTable, openTrainerDate]) => {
+         const accIdnumber = Number(accId);
+
+        const datePlusFive = new Date(dateOpenTable);
+        datePlusFive.setMinutes(datePlusFive.getMinutes() + 5);
+        const newDatePlusFiveString = datePlusFive.toISOString();
+        
+        const dateMinusFive = new Date(dateOpenTable);
+        dateMinusFive.setMinutes(dateMinusFive.getMinutes() - 5);
+        const newDateMinusFiveString = dateMinusFive.toISOString();
+
+        chrome.tabs.create({
+                // url: `https://loggia.ecoinfra.io/?az=brains&ns=brain-platform&log=${dateGame}&q=node~eq~${brainServer}&q=component~eq~system&text=${accIdnumber}+${gameId}`
+                url: `https://loggia.ecoinfra.io/?az=brains&ns=brain-platform&from=${newDateMinusFiveString}&to=${newDatePlusFiveString}&q=node~eq~${brainServer}&q=component~eq~system&text=${accIdnumber}+<opentable`
+         });
+    });
+  });
+
+
+// Кнопка мозговых логов на открытие сессии
+button5.addEventListener('click', function() {
+    extractUrlParams(([gameId, accId, gameDate, brainServer, dateOpenTable, openTrainerDate]) => {
+
+                const accIdnumber = Number(accId);
+                
+                const datePlusFive = new Date(openTrainerDate);
+                datePlusFive.setMinutes(datePlusFive.getMinutes() + 5);
+                const newDatePlusFiveString = datePlusFive.toISOString();
+                
+                const dateMinusFive = new Date(openTrainerDate);
+                dateMinusFive.setMinutes(dateMinusFive.getMinutes() - 5);
+                const newDateMinusFiveString = dateMinusFive.toISOString();
+
+                // Пример использования массива: создание новой вкладки с изменённым URL
+                   chrome.tabs.create({
+                // url: `https://loggia.ecoinfra.io/?az=brains&ns=brain-platform&log=${dateGame}&q=node~eq~${brainServer}&q=component~eq~system&text=${accIdnumber}+${gameId}`
+                url: `https://loggia.ecoinfra.io/?az=brains&ns=brain-platform&from=${newDateMinusFiveString}&to=${newDatePlusFiveString}&q=component~eq~system&text=${accIdnumber}+<opentrainer`
+         });
+               
+               });
+        });
+
+        
+// Кнопка логов импорта
+button6.addEventListener('click', function() {
+    extractUrlParams(([gameId, accId, gameDate, brainServer, dateOpenTable, openTrainerDate]) => {
+
+                const accIdnumber = Number(accId);
+                
+                const datePlusFive = new Date(openTrainerDate);
+                datePlusFive.setMinutes(datePlusFive.getMinutes() + 120);
+                const newDatePlusFiveString = datePlusFive.toISOString();
+                
+                const dateMinusFive = new Date(openTrainerDate);
+                dateMinusFive.setMinutes(dateMinusFive.getMinutes() - 5);
+                const newDateMinusFiveString = dateMinusFive.toISOString();
+
+                // Пример использования массива: создание новой вкладки с изменённым URL
+                   chrome.tabs.create({
+                // url: `https://loggia.ecoinfra.io/?az=brains&ns=brain-platform&log=${dateGame}&q=node~eq~${brainServer}&q=component~eq~system&text=${accIdnumber}+${gameId}`
+                url: `https://loggia.ecoinfra.io/?ns=mga-platform&app=mgaimport%7Emgaerrimport&from=${newDateMinusFiveString}&to=${newDatePlusFiveString}&q=component~eq~system&text=${accIdnumber}`
+         });
+               
+               });
+        });
 
     // Парсинг ссылки в ХИ
     function extractUrlParams(callback) {
@@ -94,8 +164,12 @@ button3.addEventListener('click', function() {
                     const accId = parsedUrl.searchParams.get('accId');
                     const gameDate = parsedUrl.searchParams.get('gameDate');
                     const brainServer = parsedUrl.searchParams.get('serverCode');
-                    
-                    return [gameId, accId, gameDate, brainServer];
+                    const dateOpenTable = parsedUrl.searchParams.get('openTableDate');
+                    const openTrainerDate = parsedUrl.searchParams.get('openTrainerDate');
+                    console.log(gameDate);
+                    console.log(openTrainerDate);
+                    console.log(dateOpenTable);
+                    return [gameId, accId, gameDate, brainServer, dateOpenTable, openTrainerDate];
                 }
             }, (results) => {
                 if (results && results[0] && results[0].result) {
